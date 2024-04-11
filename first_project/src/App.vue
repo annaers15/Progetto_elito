b15e4450d3b0f5fa13ae5586066a9c22c851420e
Anna Volante <annaers97@gmail.com>
2024-04-11 17:21:26 +0200

    Elito project

---
+++ first_project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <PricingPlan />
+    <TalkComponent />
+    <NewsComponent />
+    <ContactUsComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import PricingPlan from './components/PricingPlan.vue'
+import TalkComponent from './components/TalkComponent.vue'
+import NewsComponent from './components/NewsComponent.vue'
+import ContactUsComponent from './components/ContactUsComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PricingPlan,
+    TalkComponent,
+    NewsComponent,
+    ContactUsComponent,
+    FooterComponent
+    
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+#app {
+  font-family: "Inter", sans-serif;
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ first_project/src/components/TalkComponent.vue
@@ -0,0 +1,153 @@
+<template>
+    <section id="talk">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="title-area">
+                        <h3 class="d-flex justify-content-center">Let’s Talk</h3>
+                        <p class="d-flex justify-content-center">Must explain to you how all this mistaken idea of
+                            denouncing <br>pleasure born and give you a complete account the system</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col lg-8">
+                    <div class="container">
+                        <div class="box-contact">
+
+                            <!-- Caselle Input -->
+                            <h4 class="d-flex justify-content-center">Get In Touch</h4>
+                            <!-- nome -->
+                            <input type="text" id="fname" name="fname" placeholder="Your Name">
+                            <!-- mail -->
+                            <input type="email" id="fname" name="fname" placeholder="Your E-mail"> <br>
+                            <!-- phone -->
+                            <input type="phone" value="" name="phone" placeholder="Your phone"><br>
+                            <!-- Services -->
+                            <select name="subject" type="text">
+                                <option value="">Choise a Service</option>
+                                <option value="">Web Design</option>
+                                <option value="">Web Development</option>
+                                <option value="">Marketing</option>
+                            </select>
+
+                            <!-- textarea -->
+                            <div class="mb-3">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                    placeholder="Message"></textarea>
+                            </div>
+                            <button type="button" class="btn btn-primary btn-lg">Submit Now <svg
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg></button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col lg-4">
+                    <div class="box">
+                        <div class="container">
+                            <h5>25 North Street,Dubai</h5>
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                <path
+                                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                            </svg> Office Address</span>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <div class="container">
+                            <h5>info@elito.com</h5>
+                            <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-envelope" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                            </svg> Official Mail</span>
+                       </div>
+                        
+                    </div>
+                    <div class="box">
+                       <div class="container">
+                         <h5>+91 256-987-239</h5>
+                         <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-telephone" viewBox="0 0 16 16">
+                                <path
+                                    d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                            </svg> Official Phone</span>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'TalkComponent'
+}
+</script>
+
+<style scoped>
+#talk {
+    background-color: #131313;
+    color: white;
+    padding: 90px;
+}
+
+p {
+    color: rgba(238, 237, 237, 0.794);
+    font-size: 18px;
+    line-height: 2.0;
+    padding-bottom: 30px;
+}
+
+h3 {
+    font-size: 48px;
+}
+
+h4,
+h5 {
+    font-size: 30px;
+    padding-bottom: 10px;
+}
+
+/* per creare il box */
+.container .box-contact {
+    background-color: #262626;
+    padding: 30px;
+}
+
+/* caselle di input */
+input,
+select,
+.textarea {
+    padding: 8px;
+    margin: 10px;
+    background-color: #131313;
+    border-bottom: solid, rgba(206, 206, 206, 0.567), 0.5px;
+}
+
+.box-contact button {
+    background-color: #59c378;
+    text-align: center;
+    border-color: #59c378;
+    border-radius: 40px;
+    padding: 10px;
+}
+
+.box .container {
+    background-color: #262626;
+    padding: 30px;
+    margin-bottom:25px ;
+}
+
+.box svg {
+    color: #ffc107;
+    font-size: 50px; 
+}
+</style>
